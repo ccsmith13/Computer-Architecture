89324fcc347d4e9728f9b794319bcd1bbdd056d1
Christine Smith <christinecsmith1@gmail.com>
2020-12-08 19:15:12 -0800

    push and pop for stack.ls8

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -5,6 +5,9 @@ import sys
 HLT = 0b00000001		
 LDI = 0b10000010
 PRN = 0b01000111
+PUSH = 0b01000111
+POP = 0b01000110
+
 
 class CPU:
     """Main CPU class."""
@@ -13,6 +16,7 @@ class CPU:
         """Construct a new CPU.""" 
         self.ram = [0] * 256
         self.reg = [0] * 8
+        # set the stack pointer 
         self.reg[7] = 0xF4
 
         self.pc = 0
@@ -90,18 +94,43 @@ class CPU:
     def run(self):
         """Run the CPU."""
         while self.running:
+
             command_to_execute = self.ram_read(self.pc)
 
             if command_to_execute == LDI:
                 operation_1 = self.ram_read(self.pc + 1)
                 operation_2 = self.ram_read(self.pc + 2)
-
                 self.reg[operation_1] = operation_2
                 self.pc += 3
-            if command_to_execute == PRN:
+
+            elif command_to_execute == PRN:
                 operation_1 = self.ram_read(self.pc + 1)
                 print(self.reg[operation_1])
                 self.pc += 2
-            if command_to_execute == HLT:
+
+            elif command_to_execute == HLT:
                 self.running = False
                 self.pc += 1
+
+            elif command_to_execute == PUSH: 
+                # decrement the SP 
+                self.reg[7] -= 1
+                # starts at F4, decrements to F3 
+                # copy value from given register in address SP is pointing to 
+                reg_address = self.ram[self.pc+1]
+                value = self.reg[reg_address]
+
+                # copy into SP address 
+                SP = self.reg[7]
+                self.ram[SP] = value
+            
+            elif command_to_execute == POP: 
+                SP = self.reg[7]
+                value = self.ram[SP]
+                reg_address = self.ram[self.pc+1]
+                self.reg[reg_address] = value
+                self.reg[7] += 1
+                
+
+
+
